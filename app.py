1a98bcd780fb60fda0c0f396d9ab9dc0b17dd351
marcobueso <mbueso97@gmail.com>
2020-01-27 09:41:04 -0600

    Reset Python, added /hello/

--- app.py
+++ app.py
@@ -1,23 +1,18 @@
 from flask import Flask
+from flask import render_template
+from datetime import datetime
+import re
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World!"
+    return "Hello, Flask!"
 
-@app.route("hello/<name>")
-def hello_there(name):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
-
-    # Filter the name argument to letters only using regular expressions. URL
-    # can contain arbitrary text, so we restric to safe characters only.
-    match_object = re.match("[a-zA-Z]+", name)
-
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
-    
-    content = "Hello there, " + clean_name + "! It's " + formatted_now
-    return content
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_there(name = None):
+    return render_template("hello_there.html",
+        name=name,
+        date=datetime.now()
+    )
